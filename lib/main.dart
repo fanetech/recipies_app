ca557132cda3e236437241b39f895908cacc7ac7
fanetech <louguefranck.20@gmial.com>
2024-05-09 23:45:20 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:recipies_app/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "RecipBook",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepOrange
+            ),
+            useMaterial3: true,
+            appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+      home: const LoginPage(),
+          
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Login"),
+      ),
+      body: SafeArea(child: _buildUi()),
+    );
+  }
+
+  Widget _buildUi() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _title(),
+            _loginForm(),
+          ],
+      ),
+    );
+  }
+ 
+  
+
+  Widget _title() {
+    return const Text(
+      "Recip Book",
+      style: TextStyle(
+        fontSize: 35,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.90,
+      height: MediaQuery.sizeOf(context).height * 0.30,
+      child: Form(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Username",
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Password",
+              ),
+            ),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.60,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text("Login"),
+      ),
+    );
+  }
+}
